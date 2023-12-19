be881c55daec28689ee81aa729002d6c2e2f3855
Denis Mukhin <denis.m.mukhin@gmail.com>
2023-12-19 20:30:34 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,9 +5,18 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-
         System.out.println("Размер массива ages: " + ages.length);
         System.out.println("Размер массива surnames: " + surnames.length);
         System.out.println("Размер массива prices: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Denis Mukhin";
+        names[2] = "Ilya Oborin";
+        names[3] = "Maxim Krukov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
