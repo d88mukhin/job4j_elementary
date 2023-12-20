750d47b9203c4053a1916ea7443d7b1bc6eeb28d
Denis Mukhin <denis.m.mukhin@gmail.com>
2023-12-20 19:40:50 +0300

    6.2.2. Переставить элементы массива [#226795]

--- src/test/java/ru/job4j/array/SwitchArrayTest.java
+++ src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -37,4 +37,34 @@ class SwitchArrayTest {
         int[] excepted = new int[] {1};
         assertThat(result).containsExactly(excepted);
     }
+
+    @Test
+    void whenSwap0To3() {
+        int[] input = {1, 2, 3, 4};
+        int source = 0;
+        int destination = input.length - 1;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] excepted = {4, 2, 3, 1};
+        assertThat(result).containsExactly(excepted);
+    }
+
+    @Test
+    void whenSwap3To4() {
+        int[] input = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int source = 3;
+        int destination = 4;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] excepted = {1, 2, 3, 5, 4, 6, 7, 8, 9};
+        assertThat(result).containsExactly(excepted);
+    }
+
+    @Test
+    void whenSwap2To5() {
+        int[] input = {1, 2, 3, 4, 5, 6, 7};
+        int source = 2;
+        int destination = 5;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] excepted = {1, 2, 6, 4, 5, 3, 7};
+        assertThat(result).containsExactly(excepted);
+    }
 }
