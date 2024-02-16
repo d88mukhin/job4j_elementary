7eb3eb941dfb6fe22b351c14823ed4761899438a
Denis Mukhin <denis.m.mukhin@gmail.com>
2024-02-16 06:55:33 +0300

    5. Конвертер валюты [#41583]

---
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,34 @@
+package ru.job4j.converter;
+
+public class Converter {
+    public static float rubleToEuro(float value) {
+        float result = value / 70;
+        return result;
+    }
+
+    public static float rubleToDollar(float value) {
+        float result = value / 60;
+        return result;
+    }
+
+    public static float euroToRuble(float value) {
+        float result = value * 70;
+        return result;
+    }
+
+    public static float dollarToRuble(float value) {
+        float result = value * 60;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+        float dollar = Converter.rubleToDollar(120);
+        System.out.println("120 rubles are " + dollar + " dollars.");
+        float ruble = Converter.dollarToRuble(4);
+        System.out.println("4 dollars are " + ruble + "rubles");
+        ruble = Converter.euroToRuble(6);
+        System.out.println("6 euro are " + ruble + "rubles");
+    }
+}
