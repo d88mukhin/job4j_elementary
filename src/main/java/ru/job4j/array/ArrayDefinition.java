6558c932f6b44ab10037e2205cec39f4a88439d9
Denis Mukhin <denis.m.mukhin@gmail.com>
2024-03-18 06:00:59 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,9 +5,19 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
 
+        names[0] = "Petr";
+        names[1] = "Denis";
+        names[2] = "Oleg";
+        names[3] = "Alex";
         System.out.println("Размер массива ages : " + ages.length);
         System.out.println("Размер массива surnames : " + surnames.length);
         System.out.println("Размер массива prices : " + prices.length);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
