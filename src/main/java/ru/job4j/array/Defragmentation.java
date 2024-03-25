e6c992989fb3551671b1167d59e12f321cbb69e8
Denis Mukhin <denis.m.mukhin@gmail.com>
2024-03-25 06:03:24 +0300

    6.8. Метод двух указателей [#225]

--- src/main/java/ru/job4j/array/Defragmentation.java
+++ src/main/java/ru/job4j/array/Defragmentation.java
@@ -1,6 +1,6 @@
 package ru.job4j.array;
 
-public class Defragment {
+public class Defragmentation {
     public static int[] compress(int[] array) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == 0) {
@@ -12,6 +12,22 @@ public class Defragment {
                     }
                 }
             }
+        }
+        return array;
+    }
+
+    public static int[] compress2Point(int[] array) {
+        int i = 0;
+        int j = 0;
+        while (j < array.length) {
+            if (array[i] == 0 && array[j] != 0) {
+                array[i] = array[j];
+                array[j] = 0;
+            }
+            if (array[i] != 0) {
+                i++;
+            }
+            j++;
         }
         return array;
     }
