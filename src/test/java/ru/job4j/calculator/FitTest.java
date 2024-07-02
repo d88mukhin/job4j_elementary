ae98be1d4b884058083ae55bc5005a8e55470c75
Denis Mukhin <denis.m.mukhin@gmail.com>
2024-07-02 09:09:16 +0300

    2. Тесты для задачи "идеальный вес" [#159458]

--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -10,8 +10,8 @@ class FitTest {
     void whenMan180Then92() {
         short input = 180;
         double expected = 92;
-        double result = Fit.manWeight(input);
-        assertThat(result).isEqualTo(expected, withPrecision(0.01));
+        double output = Fit.manWeight(input);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
