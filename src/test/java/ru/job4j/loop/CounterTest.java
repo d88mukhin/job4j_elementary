19eae36859dfed84a76688182dff1270e730c557
Denis Mukhin <denis.m.mukhin@gmail.com>
2024-09-01 21:31:43 +0300

    5.0.1. Сумма чисел. [#226416]

---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CounterTest {
+
+    @Test
+    void whenStart0Finish5Then15() {
+        int start = 0;
+        int finish = 5;
+        int result = Counter.sum(start, finish);
+        int expected = 15;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStart1Finish1Then1() {
+        int start = 1;
+        int finish = 1;
+        int result = Counter.sum(start, finish);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStart8Finish3Then0() {
+        int start = 8;
+        int finish = 3;
+        int result = Counter.sum(start, finish);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+}
