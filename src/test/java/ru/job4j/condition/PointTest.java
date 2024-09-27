a457eac35d5d2e0733770c8114bc00b46530ff94
Denis Mukhin <denis.m.mukhin@gmail.com>
2024-09-27 07:21:55 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,45 +8,36 @@ class PointTest {
 
     @Test
     void when00to20Then2() throws InterruptedException {
-        Thread.sleep(5000);
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double output = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double output = a.distance(b);
         double expected = 2;
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when0Minus3to20Then3Dot61() {
-        int x1 = 0;
-        int y1 = -3;
-        int x2 = 2;
-        int y2 = 0;
-        double output = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(0, -3);
+        Point b = new Point(2, 0);
+        double output = a.distance(b);
         double expected = 3.61;
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when23to65Then4dot47() {
-        int x1 = 2;
-        int y1 = 3;
-        int x2 = 6;
-        int y2 = 5;
-        double output = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(2, 3);
+        Point b = new Point(6, 5);
+        double output = a.distance(b);
         double expected = 4.47;
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenMinus4Minus1toMinus2Minus3Then2Dot83() {
-        int x1 = -4;
-        int y1 = -1;
-        int x2 = -2;
-        int y2 = -3;
-        double output = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(-4, -1);
+        Point b = new Point(-2, -3);
+        double output = a.distance(b);
         double expected = 2.83;
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
