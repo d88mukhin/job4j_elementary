0720c7bc3fbf7b35f7e9f0d4e6c7412cc77907d2
Denis Mukhin <denis.m.mukhin@gmail.com>
2025-04-15 20:18:12 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,13 +5,10 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-
         System.out.println("Размер массива ages равен : " + ages.length);
         System.out.println("Размер массива surnames равен : " + surnames.length);
         System.out.println("Размер массива prices равен : " + prices.length);
-
         String[] names = new String[4];
-
         names[0] = "Petr Arsentev";
         names[1] = "Denis Mikhin";
         names[2] = "Sergey Burunov";
